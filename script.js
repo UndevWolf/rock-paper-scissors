e7c13b2973cae348e27b150c48c3eed4ad4b4d69
IslesPJ <isles.pj@gmail.com>
2026-03-22 13:15:20 +0900

    Unstyled, working game DOM.

--- script.js
+++ script.js
@@ -11,26 +11,22 @@ function getComputerChoice() {
         return "scissors";
     }
 }
-function getHumanChoice() {
-    let humanChoice = prompt("Enter your choice dear human: ");
-    return humanChoice.toLowerCase();
-}
 function playRound (human,computer) {
     if ((human === "rock" && computer === "scissors") || 
         (human === "paper" && computer === "rock") ||
         (human === "scissors" && computer === "paper")) {
         humanScore += 1;
         console.log("Human: " + human + " vs " + "Computer: " + computer);
-        console.log("You Win");
+        matchResult.textContent = "You Win";
      }
     else if (human == computer) {
-        console.log(human, computer);
-        console.log("Tie");
+        console.log("Human: " + human + " vs " + "Computer: " + computer);
+        matchResult.textContent = "Tie";
     }
     else {
         computerScore += 1;
-        console.log(human, computer);
-        console.log("You Lose.");
+        console.log("Human: " + human + " vs " + "Computer: " + computer);
+        matchResult.textContent = "You Lose.";
     }
 }
 let humanScore = 0;
@@ -40,15 +36,34 @@ let computerScore = 0;
 
 
 
-function playGame(rounds) {
-    for (i = 0; i < rounds; i++) {
-        console.log("Round " + (i+1))
-        let humanSelect = getHumanChoice();
-        let computerSelect = getComputerChoice();
-        playRound (humanSelect,computerSelect);
-        console.log(humanScore, computerScore);
-    }
-    console.log("Good Game.")
-}
+// function playGame(rounds) {
+//     for (i = 0; i < rounds; i++) {
+//         console.log("Round " + (i+1))
+//         let humanSelect = getHumanChoice();
+//         let computerSelect = getComputerChoice();
+//         playRound (humanSelect,computerSelect);
+//         console.log(humanScore, computerScore);
+//     }
+//     console.log("Good Game.")
+// }
 
-playGame(5);
+const choiceButton = document.querySelectorAll(".choiceButton");
+const scores = document.querySelector("#scores");
+const matchResult = document.querySelector("#result");
+
+choiceButton.forEach(choice => choice.addEventListener("click", (e) => {
+    let human = "";
+    switch (e.target.value) {
+        case "rock":
+            human = e.target.value;
+            break;
+        case "paper":
+            human = e.target.value;
+            break;
+        case "scissors":
+            human = e.target.value;
+            break;
+    }
+    playRound(human, getComputerChoice());
+    scores.textContent = humanScore + " - " + computerScore;
+}));
